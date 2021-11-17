857b597b8728a5548a352934178fc2f1ac9be9d3
Ilya Zub <282605+ilyazub@users.noreply.github.com>
2021-11-17 14:14:24 +0200

    Fetch cities of all states in Walmart

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::time::Duration;
 use tower::{Service, ServiceExt};
 
+const STATES: [&str; 59] = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "AS", "GU", "MH", "FM", "MP", "PW", "PR", "VI"];
+
 #[tokio::main]
 async fn main() {
     let mut headers = reqwest::header::HeaderMap::new();
@@ -13,14 +15,16 @@ async fn main() {
         .unwrap();
 
     let mut service = tower::ServiceBuilder::new()
-        .concurrency_limit(10)
+        .concurrency_limit(2)
         .buffer(100)
         .rate_limit(10, Duration::new(10, 0))
         .service_fn(move |req| client.execute(req));
 
-    let req = reqwest::Request::new(reqwest::Method::GET, reqwest::Url::parse("https://www.walmart.com/store/electrode/api/store-directory?st=CA").unwrap());
+    for state in STATES {
+        let req = reqwest::Request::new(reqwest::Method::GET, reqwest::Url::parse_with_params("https://www.walmart.com/store/electrode/api/store-directory", &[("st", state)]).unwrap());
 
-    let res = service.ready().await.unwrap().call(req).await.unwrap();
+        let res = service.ready().await.unwrap().call(req).await.unwrap();
 
-    println!("res: {:?}", res.text().await.unwrap());
+        println!("res: {:?}", res.text().await.unwrap());
+    }
 }
