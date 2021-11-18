2d79a985bffec54c782df4c7cdfbe5a574c40fbd
Ilya Zub <282605+ilyazub@users.noreply.github.com>
2021-11-18 18:04:54 +0200

    Actually send requests concurrently

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use futures::stream::{self, StreamExt};
 use scraper::{Html, Selector as ScraperSelector};
 use serde::{Deserialize, Serialize};
-use std::time::Duration;
+use tokio::time::{sleep, Duration};
 use tower::{Service, ServiceExt};
 
 // TODO: Get all states from https://www.walmart.com/store/directory
@@ -11,157 +12,174 @@ const STATES: [&str; 51] = [
     "WV", "WI", "WY",
 ];
 
-#[derive(Debug, Deserialize, Serialize)]
+const OUTPUT_FILE_NAME: &str = "output/walmart_stores.csv";
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
-struct City<'a> {
-    city: &'a str,
+struct City {
+    city: String,
+
+    store_id: Option<usize>,
+    store_count: Option<usize>,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+struct InnerCity {
+    state: String,
+    city: String,
 
     store_id: Option<usize>,
     store_count: Option<usize>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
+#[serde(rename_all(deserialize = "camelCase"))]
 struct Store {
     store_id: usize,
     postal_code: String,
     address: String,
 }
 
-#[tokio::main]
-async fn main() {
+type StoreLocatorError = Box<dyn std::error::Error>;
+
+#[tokio::main(worker_threads = 10)]
+async fn main() -> Result<(), StoreLocatorError> {
     let mut headers = reqwest::header::HeaderMap::new();
-    headers.insert(reqwest::header::ACCEPT, "application/json".parse().unwrap());
+    headers.insert(reqwest::header::ACCEPT, "application/json".parse()?);
 
     let client = reqwest::Client::builder()
         .user_agent("Opera/9.80 (Windows NT 6.1; WOW64) Presto/2.12.388 Version/12.18")
         .default_headers(headers)
-        .build()
-        .unwrap();
+        .build()?;
 
-    let mut service = tower::ServiceBuilder::new()
-        .concurrency_limit(50)
-        .buffer(200)
-        .rate_limit(25, Duration::new(5, 0))
-        .service_fn(move |req| client.execute(req));
+    // let mut service = tower::ServiceBuilder::new()
+    //     .concurrency_limit(200)
+    //     // .buffer(200)
+    //     .rate_limit(50, Duration::new(5, 0))
+    //     .service_fn(move |req| client.execute(req));
 
-    let mut walmart_addresses_queue = Vec::<Store>::new();
+    // let mut walmart_addresses = Vec::<Store>::new();
 
-    for state in STATES {
-        let state_req = reqwest::Request::new(
-            reqwest::Method::GET,
-            reqwest::Url::parse_with_params(
-                "https://www.walmart.com/store/electrode/api/store-directory",
-                &[("st", state)],
-            )
-            .unwrap(),
-        );
-
-        let state_res = service
-            .ready()
-            .await
-            .unwrap()
-            .call(state_req)
-            .await
-            .unwrap();
-
-        let state_text = state_res.text().await.unwrap();
-
-        let cities: Vec<City> = serde_json::from_str(&state_text).unwrap();
-
-        println!("State JSON: {:?}", cities);
-
-        for city in cities {
-            println!(
-                "Downloading Walmart stores of a city: {city:?}...",
-                city = city
-            );
-
-            if city.store_id.is_some() && city.store_count.is_none() {
-                let store_id = city.store_id.unwrap();
-
-                let store_request = reqwest::Request::new(
-                    reqwest::Method::GET,
-                    reqwest::Url::parse(&format!(
-                        "https://www.walmart.com/store/{store_id}",
-                        store_id = store_id
-                    ))
-                    .unwrap(),
-                );
-
-                let store_res = service
-                    .ready()
-                    .await
-                    .unwrap()
-                    .call(store_request)
-                    .await
-                    .unwrap();
-
-                let store_text = store_res.text().await.unwrap();
-
-                let store_html = Html::parse_document(&store_text);
-
-                let sel_store_zip_code = css(".store-address-postal[itemprop=postalCode]");
-                let sel_store_address = css(".store-address[itemprop=address]");
-
-                if let (Some(store_zip_code_ref), Some(store_address_ref)) = (
-                    store_html.select(&sel_store_zip_code).next(),
-                    store_html.select(&sel_store_address).next(),
-                ) {
-                    let store = Store {
-                        store_id: store_id,
-                        postal_code: store_zip_code_ref.text().collect(),
-                        address: store_address_ref.text().collect(),
-                    };
-
-                    println!("Found store: {store:?}", store = store);
-
-                    walmart_addresses_queue.push(store);
-                }
-            } else {
-                let city_request = reqwest::Request::new(
-                    reqwest::Method::GET,
+    let walmart_addresses: Vec<Vec<Option<Store>>> = tokio_stream::iter(STATES)
+        .map(|state| {
+            let client = &client;
+            async move {
+                let state_res = client.get(
                     reqwest::Url::parse_with_params(
                         "https://www.walmart.com/store/electrode/api/store-directory",
-                        &[("st", state), ("city", city.city)],
+                        &[("st", state)],
                     )
                     .unwrap(),
                 );
 
-                let city_res = service
-                    .ready()
-                    .await
-                    .unwrap()
-                    .call(city_request)
-                    .await
-                    .unwrap();
+                let state_text = state_res.send().await.unwrap().text().await.unwrap();
 
-                let city_text = city_res.text().await.unwrap();
+                let cities: Vec<City> = serde_json::from_str(&state_text).unwrap();
+                let inner_cities: Vec<InnerCity> = cities
+                    .iter()
+                    .map(|city| InnerCity {
+                        state: state.to_string(),
+                        city: city.city.clone(),
+                        store_id: city.store_id,
+                        store_count: city.store_count,
+                    })
+                    .collect();
 
-                let stores: Vec<Store> = serde_json::from_str(&city_text).unwrap();
+                println!("State: {state}", state = state);
+                println!("State JSON: {:?}", cities);
 
-                for store in stores {
-                    println!("Found store: {store:?}", store = store);
+                inner_cities
+            }
+        })
+        .buffer_unordered(10)
+        .flat_map(tokio_stream::iter)
+        .map(|city| {
+            let client = &client;
+            async move {
+                println!(
+                    "Downloading Walmart stores of a city: {city:?}...",
+                    city = city
+                );
 
-                    walmart_addresses_queue.push(store);
+                if let (Some(store_id), None) = (city.store_id, city.store_count) {
+                    let store_request = client.get(
+                        reqwest::Url::parse(&format!(
+                            "https://www.walmart.com/store/{store_id}",
+                            store_id = store_id
+                        ))
+                        .unwrap(),
+                    );
+
+                    let store_text = store_request.send().await.unwrap().text().await.unwrap();
+
+                    let store_html = Html::parse_document(&store_text);
+
+                    let sel_store_zip_code = css(".store-address-postal[itemprop=postalCode]");
+                    let sel_store_address = css(".store-address[itemprop=address]");
+
+                    if let (Some(store_zip_code_ref), Some(store_address_ref)) = (
+                        store_html.select(&sel_store_zip_code).next(),
+                        store_html.select(&sel_store_address).next(),
+                    ) {
+                        let store = Store {
+                            store_id,
+                            postal_code: store_zip_code_ref.text().collect(),
+                            address: store_address_ref.text().collect(),
+                        };
+
+                        println!("Found store: {store:?}", store = store);
+
+                        vec![Some(store)]
+                    } else {
+                        vec![None]
+                    }
+                } else {
+                    let city_request = client.get(
+                        reqwest::Url::parse_with_params(
+                            "https://www.walmart.com/store/electrode/api/store-directory",
+                            &[("st", city.state), ("city", city.city)],
+                        )
+                        .unwrap(),
+                    );
+
+                    let city_text = city_request.send().await.unwrap().text().await.unwrap();
+
+                    let stores: Vec<Store> = serde_json::from_str(&city_text).unwrap();
+
+                    stores
+                        .into_iter()
+                        .map(|store| {
+                            println!("Found store: {store:?}", store = store);
+
+                            Some(store)
+                        })
+                        .collect::<Vec<Option<Store>>>()
                 }
             }
-        }
-    }
+        })
+        .buffer_unordered(10)
+        .collect::<Vec<_>>()
+        .await;
 
-    write_results_to_file(&walmart_addresses_queue);
+    let walmart_addresses: Vec<Store> = walmart_addresses
+        .into_iter()
+        .flatten()
+        .filter_map(|s| s)
+        .collect();
+
+    write_results_to_file(&walmart_addresses)?;
+
+    Ok(())
 }
 
 fn css(selector: &str) -> ScraperSelector {
     ScraperSelector::parse(selector).expect("this should never trigger")
 }
 
-fn write_results_to_file(records: &[Store]) -> Result<(), Box<dyn std::error::Error>> {
-    let file_name = "/tmp/walmart_stores.csv";
-    let mut writer = csv::Writer::from_path(file_name)?;
+fn write_results_to_file(records: &[Store]) -> Result<(), StoreLocatorError> {
+    let mut writer = csv::Writer::from_path(OUTPUT_FILE_NAME)?;
 
     for record in records {
-        // When writing records with Serde using structs, the header row is written automatically.
         writer.serialize(record)?;
     }
 
@@ -170,7 +188,7 @@ fn write_results_to_file(records: &[Store]) -> Result<(), Box<dyn std::error::Er
     println!(
         "Successfully wrote {number_of_records} to {file_name}",
         number_of_records = records.len(),
-        file_name = file_name
+        file_name = OUTPUT_FILE_NAME
     );
 
     Ok(())
